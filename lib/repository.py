4cd89cdf972329faff41a1fefd1197f53cc68403
Petrov Egor <zero.sentoki@gmail.com>
2024-10-08 13:54:36 +0200

    add pytest and simple test

--- lib/repository.py
+++ lib/repository.py
@@ -1,7 +1,7 @@
 from asyncpg import Connection
 
 
-async def get_name_by_id(connection: Connection, name_id: int)->str | None:
+async def get_name_by_id(connection: Connection, name_id: int) -> str | None:
     query = """SELECT * FROM names WHERE id = $1 LIMIT 1;"""
 
     row = await connection.fetchrow(query, name_id)
